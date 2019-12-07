58100b852de7727077c16db6f79968820d5f7d45
Yauheni <tyteishi@gmail.com>
2019-12-07 17:04:11 +0300

    Added different application classes for build variants

---
+++ app/src/debug/java/com/github/kiolk/cowsandbulls/DebugApp.java
@@ -0,0 +1,6 @@
+package com.github.kiolk.cowsandbulls;
+
+import android.app.Application;
+
+public class DebugApp extends Application {
+}
---
+++ app/src/release/java/com/github/kiolk/cowsandbulls/ReleaseApp.java
@@ -0,0 +1,6 @@
+package com.github.kiolk.cowsandbulls;
+
+import android.app.Application;
+
+public class ReleaseApp extends Application {
+}
