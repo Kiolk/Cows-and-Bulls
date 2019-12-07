301e65068dd8ce72e79dbeef96e361aaede79f30
Yauheni <tyteishi@gmail.com>
2019-12-07 21:34:30 +0300

    Configured app distribution

--- app/src/release/java/com/github/kiolk/cowsandbulls/ReleaseApp.java
+++ app/src/release/java/com/github/kiolk/cowsandbulls/ReleaseApp.java
@@ -2,5 +2,15 @@ package com.github.kiolk.cowsandbulls;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
+
+import io.fabric.sdk.android.Fabric;
+
 public class ReleaseApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Fabric.with(this, new Crashlytics());
+    }
 }
