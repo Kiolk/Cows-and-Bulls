7a7c1acd8f1d766d0d41b0c24f34891d780d6c6b
Dell <trosko556@gmail.com>
2019-12-14 12:38:36 +0300

    Added view for timer and result button

--- app/src/main/java/com/github/kiolk/cowsandbulls/ui/screens/GameActivity.java
+++ app/src/main/java/com/github/kiolk/cowsandbulls/ui/screens/GameActivity.java
@@ -1,22 +1,26 @@
 package com.github.kiolk.cowsandbulls.ui.screens;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.github.kiolk.cowsandbulls.R;
 import com.github.kiolk.cowsandbulls.logic.CustomTimer;
+import com.github.kiolk.cowsandbulls.logic.TimerChange;
 import com.github.kiolk.cowsandbulls.ui.views.DisplayLayout;
 import com.github.kiolk.cowsandbulls.ui.views.KeyboardLayout;
 
-public class GameActivity extends AppCompatActivity implements KeyboardLayout.OnKeyBoardListener {
+public class GameActivity extends AppCompatActivity implements KeyboardLayout.OnKeyBoardListener, TimerChange {
 
     private CustomTimer mCustomTimer;
     private TextView mTimerTV;
     private DisplayLayout mDisplayLayout;
     private KeyboardLayout mKeyboardLayout;
     private String mInput = "";
+    private ImageView mTimerIV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +30,10 @@ public class GameActivity extends AppCompatActivity implements KeyboardLayout.On
         mDisplayLayout = findViewById(R.id.display_input);
         mKeyboardLayout = findViewById(R.id.keyboard_game);
         mKeyboardLayout.setOnKeyBoardListener(this);
+        mCustomTimer = new CustomTimer(this);
+        mTimerTV = findViewById(R.id.timerTV);
+        mTimerIV = findViewById(R.id.timerIV);
+        mTimerIV.setImageDrawable(getBaseContext().getResources().getDrawable(R.drawable.ic_timer));
     }
 
     @Override
@@ -36,16 +44,39 @@ public class GameActivity extends AppCompatActivity implements KeyboardLayout.On
 
     @Override
     public void onStartPressed() {
-
+        mCustomTimer.reset();
+        mCustomTimer.start();
     }
 
     @Override
     public void onStopPressed() {
-
+        mCustomTimer.stop();
     }
 
     @Override
     public void onEnterPressed() {
 
+    }
+
+    @Override
+    public void updateView(String text) {
+        runOnUiThread(new Runnable() {
+
+            @Override
+            public void run() {
+                (mTimerTV).setText(text);
+            }
+        });
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        this.mCustomTimer = CustomTimer.restoreTimer(savedInstanceState, this);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(mCustomTimer.saveState(outState));
     }
 }
